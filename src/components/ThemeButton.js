787c3422d83c33fc6f994438b4d3b6d9936fb567
hashirsyed <itshashirsyed@gmail.com>
2024-05-18 21:28:51 +0500

    Theme button updated

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,6 +1,5 @@
-function ThemeButton({ children, className , disabled }) {
+function ThemeButton({ children, className, disabled }) {
   return (
-    <>
       <button
         className={`${className} bg-blue-900 p-3 mt-10 text-white rounded-md duration-500 hover:border-blue-900 hover:border hover:border-solid hover:bg-transparent hover:text-blue-900 disabled:bg-gray-600 disabled:hover:text-white disabled:hover:border-none`}
         disabled={disabled}
@@ -8,7 +7,6 @@ function ThemeButton({ children, className , disabled }) {
       >
         {children}
       </button>
-    </>
   );
 }
 
