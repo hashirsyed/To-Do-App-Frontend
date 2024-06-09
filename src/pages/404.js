b3611cc1b71d9c023b43b1dce1316a4ae7ff00ed
hashirsyed <itshashirsyed@gmail.com>
2024-06-09 12:05:56 +0500

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,11 @@
-import NotFound from "../image_processing20191008-30851-fhz5mc.jpg"
+import NotFound from "../image_processing20191008-30851-fhz5mc.jpg";
 
-
-function PageNotFound (){
-    return (
-        <>
-        <img src={NotFound} className="mr-auto ml-auto w-[30%]"/>
-    </>
-    )
+function PageNotFound() {
+  return (
+    <div>
+      <img src={NotFound} className="mr-auto ml-auto w-[30%]" />
+    </div>
+  );
 }
 
 export default PageNotFound;
